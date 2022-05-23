eacee8f942df8ce1cc1d8f03f39be7414e228be4
Murod <murodnazarov150@gmail.com>
2022-05-23 21:56:09 +0500

    docs: update readme with additional information

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-// const boxWrapper = document.querySelector(".box-wrapper")
+const boxWrapper = document.querySelector(".box-wrapper")
 
-// const booksData = './data.json'
+const booksData = './data.json'
 
-// async function getBooksData(url){
-//     const res = await fetch(url)
-//     const data = await res.json()
+async function getBooksData(url){
+    const res = await fetch(url)
+    const data = await res.json()
 
-//     // return console.log(data)
-//     showBooks(data)
-// }
-// getBooksData(booksData)
-
-// function showBooks(booksDetails){
-
-//     boxWrapper.innerHTML = ''
-
-//     booksDetails.forEach(bookDetail => {
-//         const {author, title, imageLink, price,description} = bookDetail
-
-//         // console.log(author, title, imageLink, price,description)
-//         const booksEl = document.createElement("div")
-//         booksEl.classList.add("box")
-
-//         booksEl.innerHTML = `
-//             <div class="box-img">
-//               <img src="${imageLink}" alt="">
-//             </div>
-//             <div class="box-text">
-//                 <div class="box-title">${title}</div>
-//                 <div class="box-author" >${author}</div>
-//                 <div class="box-desc">${description}</div>
-//                 <div class="box-price">${price} $</div>
-//                 <div class="btns flex justify-between align-center">
-//                     <p class="show" >Show More</p>
-//                     <button class="btn" >
-//                         Add To Card
-//                     </button>
-//                 </div>
-//             </div>
-//         `
-//         boxWrapper.appendChild(booksEl)
-//     });
-// }
-
-const app = {
-    addWrapper(){
-        const wrapper = document.createElement('div')
-        document.body.prepend(wrapper)
-        wrapper.classList.add("container")
-    },
-
-    createElement(element,name, className,position){
-        name.document.createElement(element)
-        position.prepend(name)
-        name.classList.add(className)
-    },
-
-    createImg(name, className, position, src) {
-        name = document.createElement('img');
-        position.append(name);
-        name.classList.add(className);
-        name.src = src;
-        name.alt = className;
-    },
-
-    
-
+    // return console.log(data)
+    showBooks(data)
 }
+getBooksData(booksData)
+
+function showBooks(booksDetails){
+
+    boxWrapper.innerHTML = ''
+
+    booksDetails.forEach(bookDetail => {
+        const {author, title, imageLink, price,description} = bookDetail
+
+        // console.log(author, title, imageLink, price,description)
+        const booksEl = document.createElement("div")
+        booksEl.classList.add("box")
+
+        booksEl.innerHTML = `
+            <div class="box-img">
+              <img src="${imageLink}" alt="">
+            </div>
+            <div class="box-text">
+                <div class="box-title">${title}</div>
+                <div class="box-author" >${author}</div>
+                <div class="box-desc">${description}</div>
+                <div class="box-price">${price} $</div>
+                <div class="btns flex justify-between align-center">
+                    <p class="show" >Show More</p>
+                    <button class="btn" >
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+        `
+        boxWrapper.appendChild(booksEl)
+    });
+}
+
+// const app = {
+//     addWrapper(){
+//         const wrapper = document.createElement('div')
+//         document.body.prepend(wrapper)
+//         wrapper.classList.add("container")
+//     },
+
+//     createElement(element,name, className,position){
+//         name.document.createElement(element)
+//         position.prepend(name)
+//         name.classList.add(className)
+//     },
+
+//     createImg(name, className, position, src) {
+//         name = document.createElement('img');
+//         position.append(name);
+//         name.classList.add(className);
+//         name.src = src;
+//         name.alt = className;
+//     },
+
+
+
+// }
